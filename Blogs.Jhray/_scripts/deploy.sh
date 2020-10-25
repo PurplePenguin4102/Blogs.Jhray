e2952f4fd658c5414b7263f47e007a93779ac8e5
Joey <joseph.ray@workpac.com>
2020-10-25 05:16:00 -0700

    extra echo

--- Blogs.Jhray/_scripts/deploy.sh
+++ Blogs.Jhray/_scripts/deploy.sh
@@ -6,9 +6,10 @@ dotnet publish Blogs.Jhray/Blogs.Jhray.csproj
 cd /home/travis/build/PurplePenguin4102/Blogs.Jhray/Blogs.Jhray/bin/Debug/netcoreapp3.1/publish/
 echo "Initiating Git"
 git init
-git remote add deploy "deploy@jhray.com:/var/Blogs.Jhray"
+git remote add deploy "deploy@jhray.com:/var/blogs.jhray"
 git config user.name "Travis CI"
 git config user.email "joseph.h.ray@gmail.com"
 git add .
 git commit -m "Deploy"
+echo "Running git push"
 git push --force deploy master
