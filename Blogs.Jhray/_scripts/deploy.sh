6bc8f6664171fa1c078fafcb003a51939f54f5f6
Joey <joseph.ray@workpac.com>
2020-10-25 02:26:07 -0700

    setting up Travis

---
+++ Blogs.Jhray/_scripts/deploy.sh
@@ -0,0 +1,14 @@
+﻿#!bin/bash
+
+echo "Running deploy script on master branch"
+
+dotnet publish jhray.com.csproj
+cd /home/travis/build/PurplePenguin4102/Blogs.Jhray/bin/Debug/netcoreapp3.1/publish/
+echo "Initiating Git"
+git init
+git remote add deploy "deploy@jhray.com:/var/Blogs.Jhray"
+git config user.name "Travis CI"
+git config user.email "joseph.h.ray@gmail.com"
+git add .
+git commit -m "Deploy"
+git push --force deploy master
---
+++ Blogs.Jhray/_scripts/install.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+mkdir -p ~/.ssh 
+chmod 700 ~/.ssh
+echo "$Deploy_Key" | perl -pe 's/\\n/\n/g' > ~/.ssh/id_rsa
+echo "Key successfully copied to id_rsa"
+chmod 600 ~/.ssh/id_rsa
+ 
