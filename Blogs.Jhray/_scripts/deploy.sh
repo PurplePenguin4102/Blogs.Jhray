e83eb73b1e872f460098dd46e06b9096682fb525
Joey <joseph.ray@workpac.com>
2020-10-25 04:55:02 -0700

    flimmer

--- Blogs.Jhray/_scripts/deploy.sh
+++ Blogs.Jhray/_scripts/deploy.sh
@@ -3,7 +3,7 @@
 echo "Running deploy script on master branch"
 
 dotnet publish Blogs.Jhray/Blogs.Jhray.csproj
-cd /home/travis/build/PurplePenguin4102/Blogs.Jhray/bin/Debug/netcoreapp3.1/publish/
+cd /home/travis/build/PurplePenguin4102/Blogs.Jhray/Blogs.Jhray/bin/Debug/netcoreapp3.1/publish/
 echo "Initiating Git"
 git init
 git remote add deploy "deploy@jhray.com:/var/Blogs.Jhray"
