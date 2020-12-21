3fb04ed2405e7056694861a20a5bd46bab9f64db
Joey <joseph.ray@workpac.com>
2020-12-21 14:45:34 -0800

    modifying deploy script

--- Blogs.Jhray/_scripts/deploy.sh
+++ Blogs.Jhray/_scripts/deploy.sh
@@ -6,7 +6,7 @@ dotnet publish Blogs.Jhray/Blogs.Jhray.csproj
 cd /home/travis/build/PurplePenguin4102/Blogs.Jhray/Blogs.Jhray/bin/Debug/netcoreapp3.1/publish/
 echo "Initiating Git"
 git init
-git remote add deploy "root@jhray.com:/var/blogs.jhray"
+git remote add deploy "deploy@jhray.com:/var/blogs.jhray"
 git config user.name "Travis CI"
 git config user.email "joseph.h.ray@gmail.com"
 git add .
